b7f836a338fb7e516d1fce0183a3950762c60b3b
Luis <lp2314p@gmail.com>
2022-10-28 06:10:32 -0500

    update

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -4,7 +4,7 @@ const networkConfig = {
     5: {
         name: "Goerli",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
-        entranceFee: ethers.utils.parseEther("0.1"),
+        entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         subscriptionId: "4953",
         callbackGasLimit: "500000",
@@ -12,7 +12,7 @@ const networkConfig = {
     },
     31337: {
         name: "hardhat",
-        entranceFee: ethers.utils.parseEther("0.1"),
+        entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         callbackGasLimit: "500000",
         interval: "30",
